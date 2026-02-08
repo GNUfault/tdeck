f93312842865f141f0702b405ec00bfdbd4e478b
Connor Thomson <blumatrikz@gmail.com>
2026-02-07 19:24:23 -0800

    Fix it (oh come on I don't really know C pee pee)

--- tdeck.cpp
+++ tdeck.cpp
@@ -20,6 +20,7 @@
 
 #include <Arduino.h>
 #include <SPI.h>
+#include "tdeck.h"
 
 #define BOARD_POWERON   10
 #define BOARD_SDCARD_CS 39
@@ -29,21 +30,18 @@
 #define BOARD_SPI_SCK   40
 #define BOARD_SPI_MOSI  41
 
-class TDeck {
-public:
-  void init(void) {
-    pinMode(BOARD_POWERON, OUTPUT);
-    digitalWrite(BOARD_POWERON, HIGH);
+void TDeck::init(void) {
+  pinMode(BOARD_POWERON, OUTPUT);
+  digitalWrite(BOARD_POWERON, HIGH);
 
-    pinMode(BOARD_SDCARD_CS, OUTPUT);
-    pinMode(RADIO_CS_PIN, OUTPUT);
-    pinMode(BOARD_TFT_CS, OUTPUT);
+  pinMode(BOARD_SDCARD_CS, OUTPUT);
+  pinMode(RADIO_CS_PIN, OUTPUT);
+  pinMode(BOARD_TFT_CS, OUTPUT);
 
-    digitalWrite(BOARD_SDCARD_CS, HIGH);
-    digitalWrite(RADIO_CS_PIN, HIGH);
-    digitalWrite(BOARD_TFT_CS, HIGH);
+  digitalWrite(BOARD_SDCARD_CS, HIGH);
+  digitalWrite(RADIO_CS_PIN, HIGH);
+  digitalWrite(BOARD_TFT_CS, HIGH);
 
-    pinMode(BOARD_SPI_MISO, INPUT_PULLUP);
-    SPI.begin(BOARD_SPI_SCK, BOARD_SPI_MISO, BOARD_SPI_MOSI);
-  }
-};
+  pinMode(BOARD_SPI_MISO, INPUT_PULLUP);
+  SPI.begin(BOARD_SPI_SCK, BOARD_SPI_MISO, BOARD_SPI_MOSI);
+}
